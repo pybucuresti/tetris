0dd3e9c4ff56a6755b7881de3b0f8691e32adf53
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2010-09-30 20:49:44 +0300

    MyRect class

--- tetris/cmd.py
+++ tetris/cmd.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+from random import randint
 
 ROOT_PATH = os.path.dirname(__file__) + '/'
 square_size = 16
@@ -15,26 +16,40 @@ UP_ARROW = 273
 LEFT_ARROW = 276
 DOWN_ARROW = 274
 RIGHT_ARROW = 275
+
+
+class MyRectangle(object):
+    def __init__(self, square):
+        self.posX = randint(0, num_horiz_squares - 1)
+        self.posY = 0
+        self.square = square
+
+    def blit_to(self, screen):
+        rect = self.square.get_rect().move([self.posX*square_size, self.posY*square_size])
+        screen.blit(self.square, rect)
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(screen_size)
 
     blue_square = pygame.image.load(ROOT_PATH + 'images/square-16x16-blue.png')
-    blue_rect = blue_square.get_rect()
+    #blue_rect = blue_square.get_rect()
     pygame.time.set_timer(pygame.USEREVENT, 500)
+    my_rect = MyRectangle(blue_square)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYUP:
-                if event.key == DOWN_ARROW:
-                   blue_rect = blue_rect.move([0, square_size]) 
-                if event.key == LEFT_ARROW:
-                   blue_rect = blue_rect.move([-square_size, 0]) 
-                if event.key == RIGHT_ARROW:
-                   blue_rect = blue_rect.move([square_size, 0]) 
-            elif event.type == pygame.QUIT: sys.exit()
-            elif event.type == pygame.USEREVENT:
-                blue_rect = blue_rect.move(speed)
+        event = pygame.event.wait()
+        #for event in pygame.event.get():
+        if event.type == pygame.KEYUP:
+            if event.key == DOWN_ARROW:
+               my_rect.posY += 1
+            if event.key == LEFT_ARROW:
+               my_rect.posX -= 1
+            if event.key == RIGHT_ARROW:
+               my_rect.posX += 1
+        elif event.type == pygame.QUIT: sys.exit()
+        elif event.type == pygame.USEREVENT:
+            my_rect.posY += 1
 
         screen.fill(black)
-        screen.blit(blue_square, blue_rect)
+        my_rect.blit_to(screen)
         pygame.display.flip()
